5c242cbba2d573a3e886a892726fc37ca060beb4
Scott Rohde <srohde@illinois.edu>
2023-07-18 17:31:26 -0500

    Added all modules to module_library.cpp.

--- src/module_library/module_library.cpp
+++ src/module_library/module_library.cpp
@@ -3,8 +3,14 @@
 
 // Include all the header files that define the modules.
 #include "example_module.h"
+#include "elapsed_time_module.h"
+#include "solar_position_michalsky.h"
+#include "thermal_time_linear.h"
 
 creator_map testBML::module_library::library_entries =
 {
-    {"example_module", &create_mc<example_module>}
+ {"example_module", &create_mc<example_module>},
+ {"elapsed_time_module", &create_mc<elapsed_time_module>},
+ {"solar_position_michalsky", &create_mc<solar_position_michalsky>},
+ {"thermal_time_linear", &create_mc<thermal_time_linear>}
 };
